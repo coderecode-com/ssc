ecd6c5be4833601703bf0d79961470a72e37f87d
eupendra <eupendra@gmail.com>
2021-11-24 20:34:49 +0530

    aspx

--- get_vs_post/get_vs_post/settings.py
+++ get_vs_post/get_vs_post/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'get_vs_post.spiders'
 #USER_AGENT = 'get_vs_post (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
