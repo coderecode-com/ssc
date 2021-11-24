ce5a18a5734ebf1eb4002c6d3dc53244d016e87e
eupendra <eupendra@gmail.com>
2021-11-24 20:11:34 +0530

    submit

---
+++ get_vs_post/get_vs_post/spiders/login_form_request.py
@@ -0,0 +1,45 @@
+import scrapy
+import json
+from scrapy import FormRequest
+
+
+class PostRequestSpider(scrapy.Spider):
+    name = 'login_form_request'
+
+    start_urls = ['https://quotes.toscrape.com/login']
+
+    def parse(self, response):
+        self.logger.info('Logging In.')
+        token = response.xpath('//input[@name="csrf_token"]/@value').get()
+        url = 'https://quotes.toscrape.com/login'
+        data = {
+            'csrf_token': token,
+            'username': 'upendra',
+            'password': 'secret'
+        }
+        yield FormRequest(url, formdata=data, callback=self.after_login)
+
+    def after_login(self, response):
+        # check login succeed before going on
+        logout_link = response.xpath('//a[text()="Logout"]/@href').get()
+        if not logout_link:
+            self.logger.error("Login failed")
+            return
+        self.logger.info("Logged in.")
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
+            }
+
+        yield response.follow(logout_link,
+                              callback=self.after_logout,
+                              dont_filter=True)
+
+    def after_logout(self, response):
+        login_link = response.xpath('//a[text()="Login"]/@href').get()
+        if login_link:
+            self.logger.info('Logged out')
+        else:
+            self.logger.error('Logout failed')
