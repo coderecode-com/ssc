1b35bda21b08eb018dfa9587ec58daa332af64f8
Upendra <eupendra@gmail.com>
2022-01-07 13:55:19 +0530

    Gutenberg

--- 18/data_from_multiple_sites/data/spiders/books.py
+++ 18/data_from_multiple_sites/data/spiders/books.py
@@ -3,24 +3,36 @@ import scrapy
 
 class BooksSpider(scrapy.Spider):
     name = 'books'
-    allowed_domains = ['books.toscrape.com']
+    allowed_domains = ['books.toscrape.com',
+                       'www.gutenberg.org']
     start_urls = ['https://books.toscrape.com/']
 
     def parse(self, response):
-        for s in response.xpath('//article')[:2]:
+        for s in response.xpath('//article'):
             item = {}
             item['price'] = s.xpath('.//p[@class="price_color"]/text()').get()
-            item['link'] = response.urljoin(s.xpath('.//h3/a/@href').get())
-            footer_text = response.xpath('//*[@class="current"]/text()').get()
-            yield scrapy.Request(item['link'],
-                                 cb_kwargs={
-                                     'item': item,
-                                     'footer': footer_text
-            },
-                callback=self.parse_book_details)
+            link = response.urljoin(s.xpath('.//h3/a/@href').get())
 
-    def parse_book_details(self, response, item,footer):
-        # print(item)
-        # print(footer)
+            yield scrapy.Request(link,
+                                 cb_kwargs={
+                                     'item': item
+                                 },
+                                 callback=self.parse_book_details)
+
+    def parse_book_details(self, response, item):
+        item['Title'] = response.xpath('//article//h1/text()').get()
         item['UPC'] = response.xpath('//*[text()="UPC"]/following-sibling::*/text()').get()
+        url = 'https://www.gutenberg.org/ebooks/search/?query={}&submit_search=Go%21'
+        yield scrapy.Request(url.format(item['Title']),
+                             cb_kwargs={
+                                 'item': item
+                             },
+                             callback=self.parse_gutenberg)
+
+    def parse_gutenberg(self, response, item):
+        number_of_matches = len(response.xpath('//li[@class="booklink"]').getall())
+        item['Gutenberg_Matches'] = number_of_matches
         yield item
+
+
+from scraper_helper import run_spider
